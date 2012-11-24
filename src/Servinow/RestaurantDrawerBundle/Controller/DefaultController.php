d8da1af42c063ebbec58ab0b1cf3eefe78b45a62
José Cabo <folcwine@gmail.com>
2012-11-24 23:05:24 +0100

    Added Navigation Bundle

--- src/Servinow/RestaurantDrawerBundle/Controller/DefaultController.php
+++ src/Servinow/RestaurantDrawerBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($restaurantID)
     {
-        return $this->render('ServinowRestaurantDrawerBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('ServinowRestaurantDrawerBundle:Default:index.html.twig', array('name' => $restaurantID));
     }
 }
