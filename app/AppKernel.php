108b114b4a15b328ed2fef14a1b2a947c6ff694a
Carlos <escrichov@gmail.com>
2012-11-26 22:27:55 +0100

    Generado el Bundle de Entidades.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Servinow\NavigationBundle\ServinowNavigationBundle(),
             new Servinow\MovimientosDeCajaBundle\ServinowMovimientosDeCajaBundle(),
+            new Servinow\EntitiesBundle\ServinowEntitiesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
