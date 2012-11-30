b76a5baece69e67fe82d0212a2fb554572c2fc87
José Cabo <folcwine@gmail.com>
2012-11-30 15:35:22 +0100

    Little modifications in navigation. Now base navigation needs the restaurantID.

--- src/Servinow/NavigationBundle/Controller/NavigationController.php
+++ src/Servinow/NavigationBundle/Controller/NavigationController.php
@@ -6,12 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class NavigationController extends Controller {
 
-	public function indexAction() {
+	public function indexAction($restaurantID) {
 		//TODO
-		return $this->render('ServinowNavigationBundle:Navigation:index.html.twig');
+		return $this->render('ServinowNavigationBundle:Navigation:index.html.twig', array(
+			'restaurantID' => $restaurantID
+		));
 	}
 
-	public function userAccountBlockAction() {
+	public function userAccountBlockAction($restaurantID) {
 		//TODO
 		return $this->render('ServinowNavigationBundle:Navigation:userAccountBlock.html.twig',
 				array(
