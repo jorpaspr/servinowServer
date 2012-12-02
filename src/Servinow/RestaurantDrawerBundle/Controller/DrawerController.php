a6204b18ea9cd2c8a10f6cc1ea164a991b6f1746
José Cabo <folcwine@gmail.com>
2012-12-02 23:41:59 +0100

    Redone all javascript code for restaurant drawer with a much better structure.

--- src/Servinow/RestaurantDrawerBundle/Controller/DrawerController.php
+++ src/Servinow/RestaurantDrawerBundle/Controller/DrawerController.php
@@ -8,8 +8,41 @@ class DrawerController extends Controller {
 
 	public function indexAction($restaurantID) {
 		//TODO
+		
+		$drawerKnowledge = array(
+			'objects' => array(
+				array(
+					'id' => 1,
+					'x' => 0,
+					'y' => 0,
+					'wide' => 1,
+					'tall' => 1,
+					'name' => 'portón'
+				),
+				array(
+					'id' => 2,
+					'x' => 7,
+					'y' => 4,
+					'wide' => 2,
+					'tall' => 1,
+					'name' => 'ventana'
+				),
+				array(
+					'id' => 3,
+					'x' => 0,
+					'y' => 3,
+					'wide' => 1,
+					'tall' => 4,
+					'name' => 'barr2a'
+				),
+			),
+			'tables' => array()
+		);
+		
 		return $this->render('ServinowRestaurantDrawerBundle:Drawer:index.html.twig'
-						, array('restaurantID' => $restaurantID));
+						, array('restaurantID' => $restaurantID,
+								'drawerKnowledge' => json_encode($drawerKnowledge)
+							));
 	}
 
 }
---
+++ src/Servinow/RestaurantDrawerBundle/Resources/public/js/Drawer.js
@@ -0,0 +1,216 @@
+if(!_servinow)
+	var _servinow = {};
+
+(function(servinow){
+	servinow.Drawer = function(){
+		var knowledge = {
+			objects:[],
+			tables:[]
+		};
+		
+		var canvasContainer = $('#drawerCanvasContainer');
+		var canvasEl = $('#drawerCanvas');
+		var ctx;
+		
+		var objectDef = {
+			def: $('#formDraggers_object .objectElement'),
+			defTitle: $('#formDraggers_object .objectElement .title'),
+			defHandler: $('#formDraggers_object .objectElement .handler'),
+			exampleObject: $('#exampleObject').children('.objectElement'),
+			tallInput: $('#formDraggers_object .tall input'),
+			wideInput: $('#formDraggers_object .wide input'),
+			width: 40,
+			height: 40
+		};
+		
+		var colWidth;
+		var rowHeight;
+		var nCols = 15;
+		var nRows = 10;
+		
+		this.start = function(){
+			var goodStart = true;
+			
+			if(!canvasEl[0].getContext)
+				goodStart = false;
+			
+			ctx = canvasEl[0].getContext('2d');
+			if(goodStart && !ctx)
+				goodStart = false;
+			
+			if(goodStart) {
+				prepareCanvas();
+				prepareInterfaceEvents();
+				
+				loadElements();
+			}
+			
+			return goodStart;
+		}
+		
+		this.sendOnClick = function(el){
+			el.click(function(e){
+				console.log(knowledge);
+			});
+		}
+		
+		var prepareCanvas = function(){
+			var width = canvasEl.prop('width');
+			var height = canvasEl.prop('height');
+
+			colWidth = width/nCols;
+			rowHeight = height/nRows;
+
+			//Vertical lines.
+			for(var x=0; x<=nCols; x++){
+				var posx = colWidth*x;
+
+				ctx.beginPath();
+				ctx.moveTo(posx,0);
+				ctx.lineTo(posx,height);
+				ctx.stroke();
+			}
+
+			//Horizontal lines.
+			for(var y=0; y<=nRows; y++){
+				var posy = rowHeight*y;
+
+				ctx.beginPath();
+				ctx.moveTo(0,posy);
+				ctx.lineTo(width,posy);
+				ctx.stroke();
+			}
+		}
+		
+		var prepareInterfaceEvents = function(){
+			objectDef.tallInput.add( objectDef.wideInput )
+				.change(function(){
+					var wideSize = objectDef.wideInput.val();
+					var tallSize = objectDef.tallInput.val();
+					
+					objectDef.width = wideSize*colWidth;
+					objectDef.height = tallSize*rowHeight;
+					
+					objectDef.def.add( objectDef.defTitle )
+						.css({
+							'width' : objectDef.width,
+							'height': objectDef.height
+						});
+
+					if(tallSize == wideSize){
+						objectDef.def.removeClass('wide tall').addClass('square');
+					} else if(tallSize < wideSize){
+						objectDef.def.removeClass('square tall').addClass('wide');
+					} else {
+						objectDef.def.removeClass('square wide').addClass('tall');
+					}
+				});
+				
+				objectDef.def.draggable({
+					scroll: false,
+					containment: canvasContainer,
+					appendTo: canvasContainer,
+					handle: '.handle',
+					helper: function(){
+						var helper = objectDef.exampleObject.clone();
+						
+						var helperTitle = helper.find('.title').val( objectDef.defTitle.val() );
+						helper.add(helperTitle)
+							.css({
+								'width' : objectDef.width,
+								'height': objectDef.height
+							});
+						
+						return helper;
+					},
+					stop: function(e, ui){
+						var def = {
+							x: Math.floor(ui.position.left/colWidth),
+							y: Math.floor(ui.position.top/rowHeight),
+							name: objectDef.defTitle.val(),
+							wide: objectDef.wideInput.val(),
+							tall: objectDef.tallInput.val(),
+							
+							id: Math.floor(Math.random()*1000)
+						};
+						
+						addObject(def);
+					}
+				});
+		}
+		
+		var loadElements = function(){
+			var toLoad = $.parseJSON( $('#drawerKnowledge').text() );
+			
+			for(var i=0; i<toLoad.objects.length; i++){
+				addObject( toLoad.objects[i] );
+			}
+		}
+		
+		var addObject = function(def){
+			var width = def.wide*colWidth;
+			var height = def.tall*rowHeight;
+			var top = def.y*rowHeight + 5;
+			var left = def.x*colWidth + 5;
+			
+			knowledge.objects.push(def);
+			
+			var newObject = objectDef.exampleObject.clone();
+			var newObjectTitle = newObject.find('.title');
+			newObject.add( newObjectTitle )
+				.css({
+					'width' : width,
+					'height': height,
+					'top' : top,
+					'left': left
+				});
+				
+			newObjectTitle.val(def.name);
+			
+			newObject.data('id', def.id);
+			newObject.find('.removeHandler').click(function(){
+				removeObject(newObject);
+			});
+			
+			newObject.css('position', 'absolute').draggable({
+				scroll: false,
+				containment: canvasContainer,
+				appendTo: canvasContainer,
+				handle: '.handle',
+				stop: function(e, ui){
+					def.y = Math.floor(ui.position.top/rowHeight);
+					def.x = Math.floor(ui.position.left/colWidth);
+					
+					newObject.add(newObjectTitle)
+						.css({
+							top: def.y*rowHeight +5,
+							left: def.x*colWidth +5
+						});
+				}
+			});
+			
+			if(def.wide == def.tall){
+				newObject.removeClass('wide tall').addClass('square');
+			} else if(def.tall < def.wide){
+				newObject.removeClass('square tall').addClass('wide');
+			} else {
+				newObject.removeClass('square wide').addClass('tall');
+			}
+			
+			canvasContainer.append(newObject);
+		}
+		
+		var removeObject = function(el){
+			for(var i=0; i<knowledge.objects.length; i++){
+				var id = el.data('id');
+
+				if(id == knowledge.objects[i].id) { 
+					knowledge.objects.splice(i, 1);
+					el.remove();
+
+					return;
+				}
+			}
+		}
+	}
+})(_servinow);
