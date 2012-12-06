50124703120b5f1f66de3302271d7212c82d1004
Nacho Codoñer <igcogi@gmail.com>
2012-12-06 13:54:34 +0100

    Empezando a trabajar con la API que me proporciona los pedidos.

--- src/Servinow/GestionPedidosBundle/Controller/APIController.php
+++ src/Servinow/GestionPedidosBundle/Controller/APIController.php
@@ -6,8 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($restaurantID)
+    public function pedidosCamareroAction($restaurantID)
     {
+		
+		$em = $this->getDoctrine()->getEntityManager();
+		
+		$pedidos = $em->getRepository('ServinowEntitiesBundle:Pedido')->findAll();
         return $this->render('ServinowGestionPedidosBundle:GestionPedidos:index.html.twig', array(
 			'restaurantID' => $restaurantID
 		));
