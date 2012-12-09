f9b3209cc9cc409dd13f47df84e82a11ce482638
José Cabo <folcwine@gmail.com>
2012-12-09 21:29:11 +0100

    Adds full save support to the restaurant drawer and creates some entities to work with it.

--- src/Servinow/RestaurantDrawerBundle/Resources/public/js/DrawerKnowledge.js
+++ src/Servinow/RestaurantDrawerBundle/Resources/public/js/DrawerKnowledge.js
@@ -3,6 +3,7 @@ if(!_servinow)
 
 (function(servinow){
 	var url_addTable = "api/addTable/";
+	var url_saveDrawer = "api/saveDrawer/";
 	
 	servinow.DrawerKnowledge = function(){
 		
@@ -16,6 +17,18 @@ if(!_servinow)
 					def.id = data.id;
 					
 					onID();
+				}
+			});
+		}
+		
+		this.saveDrawer = function(knowledge){
+			$.ajax({
+				url: url_saveDrawer,
+				dataType: 'json',
+				type: 'POST',
+				data: knowledge,
+				success: function(data){
+					console.log("Hecho!");
 				}
 			});
 		}
