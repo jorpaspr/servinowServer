4ffca3386df78aa976b443a4787468fd20428b54
Carlos <escrichov@gmail.com>
2012-12-10 09:08:29 +0100

    Hecho el factura bundle.

--- src/Servinow/FacturaBundle/Controller/DefaultController.php
+++ src/Servinow/FacturaBundle/Controller/DefaultController.php
@@ -3,11 +3,33 @@
 namespace Servinow\FacturaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use  Servinow\EntitiesBundle\Entity\Restaurante;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($restaurantID)
     {
-        return $this->render('ServinowFacturaBundle:Default:index.html.twig', array('name' => $name));
+        // create a task and give it some dummy data for this example
+        $restaurante = $this->getDoctrine()
+                ->getRepository('ServinowEntitiesBundle:Restaurante')
+                ->find($restaurantID);
+
+        $form = $this->createFormBuilder($restaurante)
+            ->add('nombre', 'text')
+            ->add('nombreCompletoTitular', 'text')
+            ->add('NIFTitular', 'text')
+            ->add('direccion', 'text')
+            ->add('ciudad', 'text')
+            ->add('codigoPostal', 'text')
+            ->add('provincia', 'text')
+            ->add('telefonoFijo', 'text')
+            ->add('telefonoMovil', 'text')
+            ->add('fax', 'text')
+            ->getForm();
+
+        return $this->render('ServinowFacturaBundle:Default:index.html.twig', array(
+            'form' => $form->createView(),
+            'restaurantID' => $restaurantID
+        ));
     }
 }
