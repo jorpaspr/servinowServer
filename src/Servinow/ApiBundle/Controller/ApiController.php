eb4dce25cea13c0eb4d58d01a3bcbe41b1beca19
Carlos <escrichov@gmail.com>
2012-12-24 01:45:03 +0100

    Hecho el controlador de la ruta pagar.

--- src/Servinow/ApiBundle/Controller/ApiController.php
+++ src/Servinow/ApiBundle/Controller/ApiController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Servinow\EntitiesBundle\Entity\Pedido;
 use Servinow\EntitiesBundle\Entity\Restaurante;
 use Servinow\EntitiesBundle\Entity\LineaPedido;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiController extends Controller
 {
@@ -13,21 +14,32 @@ class ApiController extends Controller
     {
         $request = $this->getRequest();
         
+        $json = "";
+        
         if($request->request->has('DATA')){
             $json = $request->request->get('DATA');
         }
+        else{
+            return new Response();
+        }
         
-        $data = json_decode($json); 
+        // Returns a object (false). True -> returns a associative array
+        $data = json_decode($json, false); 
+        
+        // Gets the entity manager
+        $em = $this->getDoctrine()->getManager();
 
-        $mesa_id = $data["mesa_id"];
-        $lineas_pedido = $data["lineas_pedido"];
+        $mesa_id = $data->mesa_id;
+        $lineas_pedido = $data->lineas_pedido;
         // Conseguir la fecha de fin = fecha actual
         $fecha = date("Ymd");
         $pagado = 0;
         $confirmado = 1;
-        $restaurante = $em->getRepository('ServinowEntitiesBundle:Pedido')->find($restaurantID);
+        $restaurante = $em->getRepository('ServinowEntitiesBundle:Restaurante')->find($restaurantID);
+        $mesa = $em->getRepository('ServinowEntitiesBundle:Mesa')->find($mesa_id);
         
         $pedido = new Pedido();
+        $pedido->setMesa($mesa);
         $pedido->setConfirmado($confirmado);
         $pedido->setFecha($fecha);
         $pedido->setPagado($pagado);
@@ -35,7 +47,6 @@ class ApiController extends Controller
         
                 
         // insertar el pedido en la base de datos
-        $em = $this->getDoctrine()->getManager();
         $em->persist($pedido);
         $em->flush();
         
@@ -43,9 +54,9 @@ class ApiController extends Controller
         
         // insertar las lineas de pedido en la base de datos
         foreach ($lineas_pedido as $lp) {
-            $linea_pedido_id = $lp["linea_pedido_id"];
-            $producto_id = $lp["linea_pedido_id"];
-            $cantidad = $lp["cantidad"];
+            $linea_pedido_id = $lp->linea_pedido_id;
+            $producto_id = $lp->producto_id;
+            $cantidad = $lp->cantidad;
             $estado = "cola";
             $producto = $em->getRepository('ServinowEntitiesBundle:Producto')->find($producto_id);
             
@@ -55,13 +66,35 @@ class ApiController extends Controller
             $linea_pedido->setProducto($producto);
             $linea_pedido->setCantidad($cantidad);
             $linea_pedido->setEstado($estado);
+            $em->persist($linea_pedido);           
         }
+        $em->flush();
         
         return new Response(json_encode($id_pedido_online));
     }
     
     public function borrarAction($restaurantID)
     {
+        $request = $this->getRequest();
+        
+        $json = "";
+        if($request->request->has('DATA')){
+            $json = $request->request->get('DATA');
+        }
+        else{
+            Return new Response;
+        }
+        
+        $data = json_decode($json, false);
+        
+        // Gets the entity manager
+        $em = $this->getDoctrine()->getManager();
+        
+        $pedido_id_online = $data->pedido_id_online;
+        $mesa_id = $data->mesa_id;
+        $linea_pedido_local = $data->linea_pedido_local;
+        $cantidad = $data->cantidad;
+        
         $json_response = json_encode($datos);
         
         $response = new Response($json_response);
@@ -71,6 +104,29 @@ class ApiController extends Controller
     
     public function consultarAction($restaurantID)
     {
+        $request = $this->getRequest();
+        
+        $json = "";
+        if($request->request->has('DATA')){
+            $json = $request->request->get('DATA');
+        }
+        else{
+            Return new Response;
+        }
+        
+        $data = json_decode($json, false);
+        
+        // Gets the entity manager
+        $em = $this->getDoctrine()->getManager();
+        
+        $mesa_id = $data->mesa_id;
+        $pedido_id_online = $data->pedido_id_online;
+        
+        foreach ($pedido_id_online as $pIdOnline) {
+            $pedidoId = $pIdOnline->pedido_id_online;
+        }
+        
+        
         $json_response = json_encode($datos);
         
         $response = new Response($json_response);
@@ -80,10 +136,35 @@ class ApiController extends Controller
     
     public function pagarAction($restaurantID)
     {
-        $json_response = json_encode($datos);
+        $request = $this->getRequest();
         
-        $response = new Response($json_response);
+        $json = "";
+        if($request->request->has('DATA')){
+            $json = $request->request->get('DATA');
+        }
+        else{
+            Return new Response;
+        }
         
-        return $response;
+        $data = json_decode($json, false);
+        
+        // Gets the entity manager
+        $em = $this->getDoctrine()->getManager();
+        
+        $mesa_id = $data->mesa_id;
+        $pedido_id_online = $data->pedido_id_online;
+        
+        foreach ($pedido_id_online as $pIdOnline) {
+            $pedidoId = $pIdOnline->pedido_id_online;
+            $metodo_pago = $pIdOnline->metodo_pago;
+            
+            $pedido = $em->getRepository('ServinowEntitiesBundle:Pedido')->find($pedidoId);
+            $pedido->setMetodoPago($metodo_pago);
+            $pedido->setPagado(1);
+        }
+        
+        $em->flush();
+        
+        return new Response();
     }
 }
